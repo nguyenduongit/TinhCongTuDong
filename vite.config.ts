f56f1edeb72ebaaaa20a4ded1ed8905f3e3f1486
Nguyen Duong <nguyenduongit89@gmail.com>
2026-07-18 21:52:37 +0700

    fix: inject push event listener into vite-plugin-pwa workbox generated service worker

--- vite.config.ts
+++ vite.config.ts
@@ -36,6 +36,7 @@ export default defineConfig({
         ],
       },
       workbox: {
+        importScripts: ['/push-sw.js'],
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         runtimeCaching: [
           {
